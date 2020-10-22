30b6bc98567de24a0cf15a3dedee96c38c55cb8d
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-22 14:49:32 -0400

    added car route to the app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 require('dotenv').config();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const carRouter =require('./Cars/carRoutes')
 const app = express();
 
 
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cars', carRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
