9ab61629a15f15a189b3a580c7e4fd52d9e40a03
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-22 15:39:08 -0400

    added route to controller

--- Cars/carRoutes.js
+++ Cars/carRoutes.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
 const Car = require('./models/Car');
 
+const{getAllCars}=require('./controllers/carController');
+const { get } = require('mongoose');
 
-router.get('/get-cars',(req,res)=>{
-    Car.find().then((foundCar)=>{
-        return res.render('main/index',{
-            carsList : foundCar
-        })
-    }).catch((err)=>{
-      return res.status(400).json({ confirmation: 'fail', message: 'Error', err })
-    })
-})
+router.get('/get-cars', getAllCars)
 
-
+module.exports =router
